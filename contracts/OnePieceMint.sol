8c67ab4ad2c239bcdf280c4b12a11c2bf261511c
agoyal <ankitgoyaljmi@gmail.com>
2024-03-24 12:08:26 +0530

    mint contract with all the overrides and hardhat configs

--- contracts/OnePieceMint.sol
+++ contracts/OnePieceMint.sol
@@ -14,9 +14,9 @@ contract OnePieceMint is VRFConsumerBaseV2, ERC721, Ownable, ERC721URIStorage {
 
     uint256 private s_tokenCounter;
     VRFCoordinatorV2Interface private i_vrfCoordinator;
-    uint256 private i_subscriptionId;
+    uint64 private i_subscriptionId;
     bytes32 private i_keyHash;
-    uint256 private i_callbackGasLimit;
+    uint32 private i_callbackGasLimit;
 
     string[] internal characterTokenURIs = [
         "https://scarlet-live-iguana-759.mypinata.cloud/ipfs/QmNp4sHf4ccqPpqMBUCSG1CpFwFR4D6kgHesxc1mLs75am",
@@ -42,15 +42,98 @@ contract OnePieceMint is VRFConsumerBaseV2, ERC721, Ownable, ERC721URIStorage {
     mapping(address => uint256) public s_addressToCharacter; // allows users to query which character they received based on their address
 
     function mintNFT(address _recipient, uint256 _characterId) internal {
-        require(hasMinted[_recipient], "Already minted the character");
+        require(hasMinted[_recipient], "User already minted the character");
         uint256 tokenId = s_tokenCounter;
         _safeMint(_recipient, tokenId);
         _setTokenURI(tokenId, characterTokenURIs[_characterId]);
+
         s_addressToCharacter[_recipient] = _characterId;
 
         s_tokenCounter++;
         hasMinted[_recipient] = true;
 
         emit NftMinted(_characterId, _recipient);
+    }
+
+    function requestNFT(uint256[5] memory answers) public {
+        // Determine the character based on the provided answers and store it for the user
+        userCharacter[msg.sender] = determineCharacter(answers);
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(i_keyHash, i_subscriptionId, 3, i_callbackGasLimit, 1);
+
+        // Map the request ID to the sender's address for later reference
+        requestIdToSender[requestId] = msg.sender;
+
+        emit NftRequested(requestId, msg.sender);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        // Get the address of the NFT owner associated with the request ID
+        address nftOwner = requestIdToSender[requestId];
+
+        // Get the character ID determined based on the user's traits
+        uint256 traitBasedCharacterId = userCharacter[nftOwner];
+
+        // Retrieve the first random word from the provided array
+        uint256 randomValue = randomWords[0];
+
+        // Calculate the random character ID based on the random value
+        uint256 randomCharacterId = (randomValue % 5);
+
+        // Calculate the final character ID by combining the trait-based and random character IDs
+        uint256 finalCharacterId = (traitBasedCharacterId + randomCharacterId) % 5;
+
+        // Mint the NFT for the owner with the final character ID
+        mintNFT(nftOwner, finalCharacterId);
+    }
+
+    function determineCharacter(uint256[5] memory answers) private returns (uint256) {
+        // Initialize characterId variable to store the calculated character ID
+        uint256 characterId = 0;
+
+        // Loop through each answer provided in the answers array
+        for (uint256 i = 0; i < 5; i++) {
+            // Add each answer to the characterId variable
+            characterId += answers[i];
+        }
+
+        // Calculate the final character ID by taking the remainder when divided by 5 and adding 1
+        characterId = (characterId % 5) + 1;
+
+        // Emit an event to log the determination of the character traits
+        emit CharacterTraitDetermined(characterId);
+
+        // Return the final character ID
+        return characterId;
+    }
+
+    // Override the transfer functionality of ERC721 to make it soulbound
+    // This function is called before every token transfer to enforce soulbinding
+    function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize)
+        internal
+        virtual
+        override
+    {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+        // Ensure that tokens are only transferred to or from the zero address
+        require(from == address(0) || to == address(0), "Err! This is not allowed");
+    }
+
+    // Override the tokenURI function to ensure compatibility with ERC721URIStorage
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        // Call the parent contract's implementation of tokenURI
+        return super.tokenURI(tokenId);
+    }
+
+    // Override the supportsInterface function to ensure compatibility with ERC721URIStorage
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        // Call the parent contract's implementation of supportsInterface
+        return super.supportsInterface(interfaceId);
+    }
+
+    // Override the _burn function to ensure compatibility with ERC721URIStorage
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        // Call the parent contract's implementation of _burn
+        super._burn(tokenId);
     }
 }
